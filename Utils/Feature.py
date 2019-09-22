4a4ba4bb49cfc1de4b3155e99fa15b55e992419f
DavidLee <davidlee860314@gmail.com>
2019-09-22 16:55:33 +0800

    0.615974 score

--- Utils/Feature.py
+++ Utils/Feature.py
@@ -1,22 +1,29 @@
 import pandas as pd
 import numpy as np 
 
-def map_stat_feature(X, b, c, mean=True, max=True, \
-        min=True, std=True, var=True, median=True, median_diff=True):
+def map_stat_feature(X, b, c, mean=True, max_val=True, \
+        min_val=True, std=True, var=True, median=True, median_diff=True):
     # b groupby c mean...
-    X[b + '_' + c + '_mean'] = \
-        X[c].map(X.loc[:, [b, c]].groupby(c).mean().loc[:, b].to_dict())
-    X[b + '_' + c + '_max'] = \
-        X[c].map(X.loc[:, [b, c]].groupby(c).max().loc[:, b].to_dict())
-    X[b + '_' + c + '_min'] = \
-        X[c].map(X.loc[:, [b, c]].groupby(c).mean().loc[:, b].to_dict())
-    X[b + '_' + c + '_std'] = \
-        X[c].map(X.loc[:, [b, c]].groupby(c).std().loc[:, b].to_dict())
-    X[b + '_' + c + '_var'] = X[b + '_' + c + '_std'] ** 2
-    X[b + '_' + c + '_median'] = \
-        X[c].map(X.loc[:, [b, c]].groupby(c).median().loc[:, b].to_dict())
-    X[b + '_' + c + '_median_diff'] = \
-        X[b] - X[b + '_' + c + '_median']
+    if(mean):
+        X[b + '_' + c + '_mean'] = \
+            X[c].map(X.loc[:, [b, c]].groupby(c).mean().loc[:, b].to_dict())
+    if(max_val):
+        X[b + '_' + c + '_max'] = \
+            X[c].map(X.loc[:, [b, c]].groupby(c).max().loc[:, b].to_dict())
+    if(min_val):
+        X[b + '_' + c + '_min'] = \
+            X[c].map(X.loc[:, [b, c]].groupby(c).mean().loc[:, b].to_dict())
+    if(std):
+        X[b + '_' + c + '_std'] = \
+            X[c].map(X.loc[:, [b, c]].groupby(c).std().loc[:, b].to_dict())
+    if(var):
+        X[b + '_' + c + '_var'] = X[b + '_' + c + '_std'] ** 2
+    if(median):
+        X[b + '_' + c + '_median'] = \
+            X[c].map(X.loc[:, [b, c]].groupby(c).median().loc[:, b].to_dict())
+    if(median_diff):
+        X[b + '_' + c + '_median_diff'] = \
+            X[b] - X[b + '_' + c + '_median']
     return None
 
 def trade_ratio(X, b, c):
@@ -29,9 +36,14 @@ class FeatureEngineer():
     def __init__(self, ):
         return None
     
-    # 1.
+    # def get_hour(self, df):
+    #     df['time'] = df['time'].astype(int).astype(str).str.zfill(6)
+    #     df['hour'] = df['time'].str.slice(0, 2).astype(int)
+    #     df['time'] = df['time'].astype('int64')
+    #     return None
+
     def numerical_stat(self, df, num_col, columns, \
-        mean=True, max=True, min=True, std=True, var=True, median=True, median_diff=True):
+        mean=True, max_val=True, min_val=True, std=True, var=True, median=True, median_diff=True):
         '''
         Mapping numerical feature to categorical feature, and get statistical value
 
@@ -48,93 +60,107 @@ class FeatureEngineer():
         if(not isinstance(columns, list)):
             raise ValueError('columns params need to be a list')
         for col in columns:
-            map_stat_feature(df, num_col, col, mean=mean, max=max, min=min, std=std, \
+            map_stat_feature(df, num_col, col, mean=mean, max_val=max_val, min_val=min_val, std=std, \
                 var=var, median=median, median_diff=median_diff)  
         return None
     
-    # 2. 
-    def card_num(self, df):
+    def num_agg(self, df, a, b):
         '''
-        one bank has how many cards. ( 一個帳號有幾張卡 )
+        one col a has how many col b. (ex. 一個帳號有幾張卡)
+        p.s. suitable for categorical to categorical
         '''
-        df['card_num'] = df['bank'].map(df.loc[:, ['card', 'bank']].groupby('bank').count().loc[:,'card'].to_dict())
+        df[b + '_num/' + a] = df[a].map(df.loc[:, [a, b]].groupby(a).nunique().loc[:, b].to_dict())
         return None
 
-    # 4.
-    def coin_country_dominance(self, df):
+    def trade_ratio(self, df, a, b):
         '''
-        dominant coin type 貨幣在那個國家交易過幾次
+        b trade ratio (ex. 貨幣在那個國家交易過幾次)
+        p.s. suitable for categorical to categorical
         '''
-        dct = (df.groupby(['nation','coin']).size() / df.groupby('nation').size()).to_dict()
-        df['coin_country_dominance'] = df.set_index(['nation', 'coin']).index.map(dct.get)
+        dct = (df.groupby([a, b]).size() / df.groupby(a).size()).to_dict()
+        df[b + '_trade_ratio/' + a] = df.set_index([a, b]).index.map(dct.get)
         return None
 
-    # 9.
-    def money_divide_term(self, df):
-        '''
-        money / term => term_money 分期金額
-        '''
-        df['money_term'] = df['money'] / (df['term'] + 1)
-        return None
+    # def coin_bank_dominance(self, df):
+    #     '''
+    #     dominant coin type 貨幣在那個歸戶交易過幾次
+    #     '''
+    #     dct = (df.groupby(['bank','coin']).size() / df.groupby('bank').size()).to_dict()
+    #     df['coin_bank_dominance'] = df.set_index(['bank', 'coin']).index.map(dct.get)
+    #     return None
 
-    # 11.
-    def count_city_num(self, df):
-        '''
-        一個國家有幾個城市
-        '''
-        df['city_num'] = df['nation'].map(df.loc[:, ['city', 'nation']].groupby('nation').count().loc[:,'city'].to_dict())
-        return None
+    # def money_divide_term(self, df):
+    #     '''
+    #     money / term => term_money 分期金額
+    #     '''
+    #     df['money_term'] = df['money'] / (df['term'] + 1)
+    #     return None
 
-    # 12.
-    def categorical_dummy_ratio(self, df, cats, dummies):
-        '''
-        dummy variable for each categorical feature trade ratio
-        ex. nation / acquirer / bank, fallback / online / 3ds / install / excess 的交易數量比例
+    # def count_city_num(self, df):
+    #     '''
+    #     一個國家有幾個城市
+    #     '''
+    #     df['city_num'] = df['nation'].map(df.loc[:, ['city', 'nation']].groupby('nation').count().loc[:,'city'].to_dict())
+    #     return None
+
+    # def categorical_dummy_ratio(self, df, cats, dummies):
+    #     '''
+    #     dummy variable for each categorical feature trade ratio
+    #     ex. nation / acquirer / bank, fallback / online / 3ds / install / excess 的交易數量比例
         
-        cats: list for categorical features
-        dummies: list for dummy features
-        '''
-        if(not isinstance(cats, list)):
-            raise ValueError('cats params need to be a list')
-        if(not isinstance(dummies, list)):
-            raise ValueError('dummies params need to be a list')
-        for c in cats:
-            for b in dummies:
-                trade_ratio(df, b, c)
-        return None
+    #     cats: list for categorical features
+    #     dummies: list for dummy features
+    #     '''
+    #     if(not isinstance(cats, list)):
+    #         raise ValueError('cats params need to be a list')
+    #     if(not isinstance(dummies, list)):
+    #         raise ValueError('dummies params need to be a list')
+    #     for c in cats:
+    #         for b in dummies:
+    #             trade_ratio(df, b, c)
+    #     return None
 
-    # 15.
-    def week_day(self, df):
-        '''
-        week day of the trade
-        '''
-        df['week_day'] = df['date'] % 7
-        return None
+    # def week_day(self, df):
+    #     '''
+    #     week day of the trade
+    #     '''
+    #     df['week_day'] = df['date'] % 7
+    #     return None
 
 
     def engineer_all(self, df):
-        # 1.
-        high = ['acquirer', 'bank', 'card', 'coin', 'mcc', 'shop', 'city', 'nation']
-        # self.numerical_stat(df, 'money', high)
-        # self.numerical_stat(df, 'iterm', high)
+        agg = ['acquirer', 'bank', 'card', 'coin', 'mcc', 'shop', 'city', 'nation', 'status', 'trade_cat', 'pay_type', 'trade_type']
+        need_encode = ['acquirer', 'bank', 'card', 'coin', 'mcc', 'shop', 'city', 'nation']
+        
+        # number aggregation
+        print('*' * 10, 'num agg...')
+        for col in need_encode:
+            li_temp = [x for x in agg if x != col]
+            for b in li_temp:
+                if(((col == 'city') and (b == 'nation')) or ((col == 'card') and (b == 'bank'))):
+                    pass
+                else:
+                    print(f'one {col} has how many {b}')
+                    self.num_agg(df, col, b)
 
-        # 2.
-        self.card_num(df)
+        # ratio aggregation
+        print('*' * 10, 'trade ratio...')
+        for col in need_encode:
+            print(col, end=',')
+            li_temp = [x for x in agg if x != col]
+            for b in li_temp:
+                self.trade_ratio(df, col, b)
 
-        # 4.
-        self.coin_country_dominance(df)
+        # count by day
+        print('*' * 10, 'count by day...')
+        for i in np.linspace(0, 119, 120):
+            print(i, end=',')
+            for col in need_encode:
+                df.loc[(df['date'] == i + 1), col + '_count_1'] = \
+                    df[col].map(\
+                        df.loc[(df['date'] == i + 1), col].value_counts())
 
-        # 9.
-        self.money_divide_term(df)
-
-        # 11.
-        self.count_city_num(df)
-
-        # 12.
-        binary_category = ['fallback', 'online', '3ds', 'install', 'excess']
-        group_category = ['nation', 'acquirer', 'bank']
-        self.categorical_dummy_ratio(df, group_category, binary_category)
-
-        # 15.
-        self.week_day(df)
+        # money stat feature
+        print('*' * 10, 'money stat..')
+        self.numerical_stat(df, 'money', need_encode, var=False, mean=False, min_val=False)
         return None
