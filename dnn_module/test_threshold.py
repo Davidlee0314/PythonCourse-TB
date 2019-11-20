b459e6d20702434cdbf58fd3b03f840fb8388bf2
ugotsuyokunaru <36882238+ugotsuyokunaru@users.noreply.github.com>
2019-11-20 22:03:00 +0800

    fix test_threshold

--- dnn_module/test_threshold.py
+++ dnn_module/test_threshold.py
@@ -8,11 +8,13 @@ def test_threshold(infer_val_path = './test.pkl'):
     with open(infer_val_path, 'rb') as file:
         infer_val_df = pkl.load(file)
 
+    print(infer_val_df.head())
+    
     threshold_list = [ 0.05 + 0.025 * x for x in range(0, 15)]
     threshold_f1_dict = {}
     for threshold in threshold_list:
-        print('threshold_{}'.format(str(threshold)[2:6]))
-        infer_val_df['threshold_{}'.format(str(threshold)[2:6])] = infer_val_df['pred_sofmax'] > threshold
+        # print('threshold_{}'.format(str(threshold)[2:6]))
+        infer_val_df['threshold_{}'.format(str(threshold)[2:6])] = infer_val_df['pred_softax'] > threshold
         infer_val_df['threshold_{}'.format(str(threshold)[2:6])] = infer_val_df['threshold_{}'.format(str(threshold)[2:6])].astype(int)
         f1 = cm_f1_score(infer_val_df['label'], infer_val_df['threshold_{}'.format(str(threshold)[2:6])], verbose=False)
         threshold_f1_dict['threshold_{}'.format(str(threshold)[2:6])] = f1
