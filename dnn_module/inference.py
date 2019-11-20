fix softmax err

dnn_module/inference.py
@@ -28,8 +28,8 @@ def inference(model, testset_loader, opt, threshold):
         os.makedirs('./infer_val/', exist_ok=True)
     else:
         os.makedirs('./submit/', exist_ok=True)
-    # softmax = nn.Softmax(dim=1)
-    softmax = nn.LogSoftmax(dim=1)
+    softmax = nn.Softmax(dim=1)
+    # softmax = nn.LogSoftmax(dim=1)
     model.eval()  # Important: set evaluation mode
 
     ids_all = None

dnn_module/train_dnn.py
@@ -36,8 +36,8 @@ def load_checkpoint(checkpoint_path, model, optimizer):
 def eval(model, testset_loader, criterion, threshold, epoch=0):
     print('Start Evaluating ...')
     model.eval()  # Important: set evaluation mode
-    # softmax = nn.Softmax(dim=1)
-    softmax = nn.LogSoftmax(dim=1)
+    softmax = nn.Softmax(dim=1)
+    # softmax = nn.LogSoftmax(dim=1)
 
     test_loss = 0
     correct = 0